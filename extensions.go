3e7579375099a8421ab2bc998c4847bb4a8637d0
Memory Clutter <memclutter@gmail.com>
2022-08-01 00:55:05 +0300

    Added proxy extension

--- extensions.go
+++ extensions.go
@@ -1,8 +1,11 @@
 package gorequests
 
 import (
+	"fmt"
 	"github.com/hashicorp/go-retryablehttp"
+	"math/rand"
 	"net/http"
+	"net/url"
 	"time"
 )
 
@@ -29,3 +32,25 @@ func (e RetryExtension) ClientOverride(c *http.Client) (*http.Client, error) {
 }
 
 func (e RetryExtension) RequestOverride(req *http.Request) (*http.Request, error) { return req, nil }
+
+// Proxies extensions
+
+type ProxiesExtension struct{ Proxies []string }
+
+func (e ProxiesExtension) ClientOverride(c *http.Client) (*http.Client, error) {
+	if len(e.Proxies) == 0 {
+		return nil, fmt.Errorf("nil proxies")
+	}
+	proxy := e.Proxies[rand.Intn(len(e.Proxies))]
+	proxyUrl, err := url.Parse(proxy)
+	if err != nil {
+		return nil, fmt.Errorf("error parse proxy url %s: %v", proxy, err)
+	}
+	return &http.Client{
+		Transport: &http.Transport{
+			Proxy: http.ProxyURL(proxyUrl),
+		},
+	}, nil
+}
+
+func (e ProxiesExtension) RequestOverride(req *http.Request) (*http.Request, error) { return req, nil }
