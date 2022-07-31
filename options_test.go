5d3bd01595dfa2f215a65abb7166847dd726d238
Memory Clutter <memclutter@gmail.com>
2022-07-31 23:23:26 +0300

    Added more tests

--- options_test.go
+++ options_test.go
@@ -63,3 +63,62 @@ func TestWithHeader(t *testing.T) {
 		assert.Equal(t, c.value, options.headers.Get(c.key), "The options.header value should be set")
 	}
 }
+
+func TestWithHeaders(t *testing.T) {
+	cases := []struct {
+		headers http.Header
+	}{
+		{headers: http.Header{
+			"Accept":       []string{"application/json"},
+			"Content-Type": []string{"form/urlencoded"},
+		}},
+	}
+
+	for _, c := range cases {
+		options := &Options{}
+		if err := WithHeaders(c.headers)(options); err != nil {
+			t.Fatal(err)
+		}
+		for k, vv := range c.headers {
+			for _, v := range vv {
+				assert.Equalf(t, v, options.headers.Get(k), "The options.header[%s] value should be set", k)
+			}
+		}
+	}
+}
+
+func TestWithOut(t *testing.T) {
+	cases := []struct {
+		outType OutType
+		out     interface{}
+	}{
+		{
+			outType: OutTypeBytes,
+			out:     &[]byte{},
+		},
+		{
+			outType: OutTypeJson,
+			out:     &map[string]string{},
+		},
+	}
+
+	for _, c := range cases {
+		options := &Options{}
+		if err := WithOut(c.out, c.outType)(options); err != nil {
+			t.Fatal(err)
+		}
+		if c.outType == OutTypeBytes {
+			if out, ok := options.out.(*[]byte); ok {
+				*out = []byte("test")
+				assert.Equal(t, []byte("test"), *(c.out.(*[]byte)), "The options.out value should be equal[bytes]")
+			} else {
+				t.Fatal("not bytes in out")
+			}
+		} else if c.outType == OutTypeJson {
+			data := map[string]string{"test": "test"}
+			newData := c.out.(*map[string]string)
+			*newData = data
+			assert.Equal(t, data, *(c.out.(*map[string]string)), "The options.out value should be equal[json]")
+		}
+	}
+}
