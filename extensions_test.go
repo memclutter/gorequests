7ee00daf9889287b0ccca294822a45601343ac2f
Memory Clutter <memclutter@gmail.com>
2022-08-01 05:16:19 +0300

    Added test for ext

--- extensions_test.go
+++ extensions_test.go
@@ -23,3 +23,20 @@ func TestRetryExtension_ClientOverride(t *testing.T) {
 	assert.Equal(t, e.RetryWaitMin, rc.RetryWaitMin, "Retry wait min should be equal")
 	assert.Equal(t, e.RetryWaitMax, rc.RetryWaitMax, "Retry wait max should be equal")
 }
+
+func TestProxiesExtension_ClientOverride(t *testing.T) {
+	e := ProxiesExtension{
+		Proxies: []string{"socks5://user:pass@host:8000"},
+	}
+	c, err := e.ClientOverride(http.DefaultClient)
+	assert.NoError(t, err, "Client override should run without error")
+	assert.IsType(t, c.Transport, &http.Transport{}, "Client should override transport")
+	tr := c.Transport.(*http.Transport)
+	proxy, err := tr.Proxy(&http.Request{})
+	assert.NoError(t, err, "Proxy should run without error")
+	pw, _ := proxy.User.Password()
+	assert.Equal(t, proxy.Scheme, "socks5", "Protocol should be equal")
+	assert.Equal(t, proxy.User.Username(), "user", "User should be equal")
+	assert.Equal(t, pw, "pass", "Password should be equal")
+	assert.Equal(t, proxy.Host, "host:8000", "Host should be equal")
+}
